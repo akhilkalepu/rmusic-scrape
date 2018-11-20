1d48b71d3937753739cb037a86c12ae644436c06
theinfamousAK <akhilkalepu@protonmail.com>
2018-11-20 02:27:31 -0500

    spotify.js

--- spotify.js
+++ spotify.js
@@ -3,16 +3,60 @@ require("dotenv").config();
 var keys = require("./keys");
 var Spotify = require("node-spotify-api");
 
+var musicArray = [];
+
+// ---------CONVERT CSV TO ARRAY---------
+
+csv();
+
+function csv() {
+    var columns = ["column1"];
+    require("csv-to-array")({
+        file: "data.csv",
+        columns: columns
+    }, function (err, array) {
+        console.log(err || array);
+        // console.log(err);
+        var musicArray = array;
+        console.log(musicArray[0].column1);
+
+        var spotify = new Spotify(keys.spotify);
+
+        for (i = 0; i < musicArray.length; i++) {
+            var query = "'" + musicArray[i].column1 + "'";
+            
+            spotify.search({
+                type: "track",
+                query: query,
+                limit: 1
+            }, function (err, data) {
+                if (err) {
+                    console.log("Error occurred: " + err);
+                    return;
+                } else {
+                    console.log("-------------------------");
+                    console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+                    console.log("Song: " + data.tracks.items[0].name);
+                    console.log("Preview link: " + data.tracks.items[0].external_urls.spotify);
+                    console.log("-------------------------");
+                }
+            });
+        }
+    });
+};
+
+// ---------------------------------------
+
 // ---------SPOTIFY THIS SONG---------
 
-spotifyThisSong();
+// spotifyThisSong();
 
 function spotifyThisSong() {
     var spotify = new Spotify(keys.spotify);
     
     spotify.search({
         type: "track",
-        query: "eleanor rigby",
+        query: "Robert Plant - Big Log",
         limit: 1
     }, function (err, data) {
         if (err) {
@@ -25,13 +69,6 @@ function spotifyThisSong() {
         console.log("Preview link: " + data.tracks.items[0].external_urls.spotify);
         console.log("Album: " + data.tracks.items[0].album.name);
         console.log("-------------------------");
-        fs.appendFile("log.txt", "\n--------------------------------------------------" + "\nSpotify This Track: " + input + "\n" + "Artist: " + data.tracks.items[0].album.artists[0].name + "\n" + "Song: " + data.tracks.items[0].name + "\n" + "Preview link: " + data.tracks.items[0].external_urls.spotify + "\n" + "Album: " + data.tracks.items[0].album.name + "\n--------------------------------------------------"),
-            function (err) {
-            if (err) {
-                console.log(err);
-            }
-        }
     });
 };
-
-// -----------------------------------------------------------
+// ---------------------------------------
