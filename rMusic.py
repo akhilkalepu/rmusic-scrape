6f7b8f8faa352687d6af3b307dd54eba45f5d862
theinfamousAK <akhilkalepu@protonmail.com>
2018-11-19 22:26:12 -0500

    files.

---
+++ rMusic.py
@@ -0,0 +1,20 @@
+import praw
+import csv
+
+reddit = praw.Reddit(client_id='RkWGlCIjkTPhUQ',
+                     client_secret='uMcdfncu3Cgws--K_65J5KDgiT0',
+                     user_agent='my user agent')
+
+top100 = []
+
+for submission in reddit.subreddit('music').top('day'):
+    if submission.link_flair_text == 'music streaming':
+        top100.append(submission.title)
+    
+print '\n'.join(top100)
+
+with open("data.csv", "w") as f:
+    wr = csv.writer(f, delimiter="\n")
+    top100 = [text.encode("utf8") for text in top100]
+    wr.writerow(top100)
+
